679f0fbc314d29cca6c602d2892ac652f7d15274
thorbenortmann <ortmann.th@gmail.com>
2021-05-15 19:57:48 +0200

    implemented AvroProducers

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -1,11 +1,12 @@
-"""Producer base-class providing common utilites and functionality"""
+"""Producer base-class providing common utilities and functionality"""
 import logging
 import time
+from concurrent.futures import Future
+from typing import Final, Dict, Optional
 
-
-from confluent_kafka import avro
+from avro.schema import SchemaFromJSONData
 from confluent_kafka.admin import AdminClient, NewTopic
-from confluent_kafka.avro import AvroProducer
+from confluent_kafka.avro import AvroProducer, CachedSchemaRegistryClient
 
 logger = logging.getLogger(__name__)
 
@@ -15,65 +16,55 @@ class Producer:
 
     # Tracks existing topics across all Producer instances
     existing_topics = set([])
+    BROKER_URL: Final[str] = 'PLAINTEXT://localhost:9092'
+    SCHEMA_REGISTRY_URL: Final[str] = 'http://localhost:8081'
 
-    def __init__(
-        self,
-        topic_name,
-        key_schema,
-        value_schema=None,
-        num_partitions=1,
-        num_replicas=1,
-    ):
+    def __init__(self,
+                 topic_name: str,
+                 key_schema: SchemaFromJSONData,
+                 value_schema: Optional[SchemaFromJSONData] = None,
+                 num_partitions: int = 1,
+                 num_replicas: int = 1,
+                 ):
         """Initializes a Producer object with basic settings"""
-        self.topic_name = topic_name
-        self.key_schema = key_schema
-        self.value_schema = value_schema
-        self.num_partitions = num_partitions
-        self.num_replicas = num_replicas
+        self.topic_name: str = topic_name
+        self.key_schema: SchemaFromJSONData = key_schema
+        self.value_schema: Optional[SchemaFromJSONData] = value_schema
+        self.num_partitions: int = num_partitions
+        self.num_replicas: int = num_replicas
 
-        #
-        #
-        # TODO: Configure the broker properties below. Make sure to reference the project README
-        # and use the Host URL for Kafka and Schema Registry!
-        #
-        #
-        self.broker_properties = {
-            # TODO
-            # TODO
-            # TODO
+        self.broker_properties: Dict[str, str] = {
+            'bootstrap.servers': self.BROKER_URL
         }
 
         # If the topic does not already exist, try to create it
         if self.topic_name not in Producer.existing_topics:
-            self.create_topic()
-            Producer.existing_topics.add(self.topic_name)
+            try:
+                self.create_topic()
+                Producer.existing_topics.add(self.topic_name)
+                logger.info(f'topic creation for {self.topic_name} successful')
+            except Exception as e:
+                logger.info(f'topic creation for {self.topic_name} failed: {e}')
 
-        # TODO: Configure the AvroProducer
-        # self.producer = AvroProducer(
-        # )
+        self.producer: AvroProducer = AvroProducer(config=self.broker_properties,
+                                                   schema_registry=CachedSchemaRegistryClient(self.SCHEMA_REGISTRY_URL),
+                                                   default_key_schema=self.key_schema,
+                                                   default_value_schema=self.value_schema)
 
-    def create_topic(self):
+    def create_topic(self) -> None:
         """Creates the producer topic if it does not already exist"""
-        #
-        #
-        # TODO: Write code that creates the topic for this producer if it does not already exist on
-        # the Kafka Broker.
-        #
-        #
-        logger.info("topic creation kafka integration incomplete - skipping")
+        client = AdminClient(self.broker_properties)
+        futures: Dict[str, Future] = client.create_topics([NewTopic(topic=self.topic_name,
+                                                                    num_partitions=self.num_partitions,
+                                                                    replication_factor=self.num_replicas)])
+        _ = [future.result() for future in futures.values()]
 
-    def time_millis(self):
-        return int(round(time.time() * 1000))
-
-    def close(self):
+    def close(self) -> None:
         """Prepares the producer for exit by cleaning up the producer"""
-        #
-        #
-        # TODO: Write cleanup code for the Producer here
-        #
-        #
-        logger.info("producer close incomplete - skipping")
+        self.producer.flush()
+        logger.info(f'closing producer for topic {self.topic_name} complete')
 
-    def time_millis(self):
+    @staticmethod
+    def time_millis() -> int:
         """Use this function to get the key for Kafka Events"""
         return int(round(time.time() * 1000))
--- producers/models/station.py
+++ producers/models/station.py
@@ -1,12 +1,14 @@
 """Methods pertaining to loading and configuring CTA "L" station data."""
+from enum import Enum
 import logging
 from pathlib import Path
+from typing import Optional
 
+from avro.schema import SchemaFromJSONData
 from confluent_kafka import avro
 
-from models import Turnstile
+from models import Turnstile, Train
 from models.producer import Producer
-
 
 logger = logging.getLogger(__name__)
 
@@ -14,68 +16,59 @@ logger = logging.getLogger(__name__)
 class Station(Producer):
     """Defines a single station"""
 
-    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_key.json")
+    key_schema: SchemaFromJSONData = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_key.json")
+    value_schema: SchemaFromJSONData = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
 
-    #
-    # TODO: Define this value schema in `schemas/station_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
-
-    def __init__(self, station_id, name, color, direction_a=None, direction_b=None):
+    def __init__(self,
+                 station_id: str,
+                 name: str,
+                 color: Enum,
+                 direction_a: Optional['Station'] = None,
+                 direction_b: Optional['Station'] = None):
         self.name = name
         station_name = (
             self.name.lower()
-            .replace("/", "_and_")
-            .replace(" ", "_")
-            .replace("-", "_")
-            .replace("'", "")
+                .replace("/", "_and_")
+                .replace(" ", "_")
+                .replace("-", "_")
+                .replace("'", "")
         )
 
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
-        topic_name = f"{station_name}" # TODO: Come up with a better topic name
+        topic_name = f"station.{station_name}"
         super().__init__(
             topic_name,
             key_schema=Station.key_schema,
-            # TODO: value_schema=Station.value_schema, # TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Station.value_schema,
+            num_partitions=1,
+            num_replicas=1
         )
 
-        self.station_id = int(station_id)
-        self.color = color
-        self.dir_a = direction_a
-        self.dir_b = direction_b
-        self.a_train = None
-        self.b_train = None
-        self.turnstile = Turnstile(self)
+        self.station_id: int = int(station_id)
+        self.color: Enum = color
+        self.dir_a: Optional['Station'] = direction_a
+        self.dir_b: Optional['Station'] = direction_b
+        self.a_train: Optional[Train] = None
+        self.b_train: Optional[Train] = None
+        self.turnstile: Turnstile = Turnstile(self)
 
-
-    def run(self, train, direction, prev_station_id, prev_direction):
+    def run(self, train: Train, direction: str, prev_station_id: int, prev_direction: str) -> None:
         """Simulates train arrivals at this station"""
-        #
-        #
-        # TODO: Complete this function by producing an arrival message to Kafka
-        #
-        #
-        logger.info("arrival kafka integration incomplete - skipping")
-        #self.producer.produce(
-        #    topic=self.topic_name,
-        #    key={"timestamp": self.time_millis()},
-        #    value={
-        #        #
-        #        #
-        #        # TODO: Configure this
-        #        #
-        #        #
-        #    },
-        #)
+        logger.info(f'Running station with id: {self.station_id}')
+        self.producer.produce(
+            topic=self.topic_name,
+            key={"timestamp": self.time_millis()},
+            value={
+                "station_id": self.station_id,
+                "train_id": train.train_id,
+                "train_status": train.status.name,
+                "direction": direction,
+                "line": self.color.name,
+                "prev_station_id": prev_station_id,
+                "prev_direction": prev_direction
+            }
+        )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "Station | {:^5} | {:<30} | Direction A: | {:^5} | departing to {:<30} | Direction B: | {:^5} | departing to {:<30} | ".format(
             self.station_id,
             self.name,
@@ -85,20 +78,20 @@ class Station(Producer):
             self.dir_b.name if self.dir_b is not None else "---",
         )
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self)
 
-    def arrive_a(self, train, prev_station_id, prev_direction):
+    def arrive_a(self, train: Train, prev_station_id: int, prev_direction: str) -> None:
         """Denotes a train arrival at this station in the 'a' direction"""
         self.a_train = train
         self.run(train, "a", prev_station_id, prev_direction)
 
-    def arrive_b(self, train, prev_station_id, prev_direction):
+    def arrive_b(self, train: Train, prev_station_id: int, prev_direction: str) -> None:
         """Denotes a train arrival at this station in the 'b' direction"""
         self.b_train = train
         self.run(train, "b", prev_station_id, prev_direction)
 
-    def close(self):
+    def close(self) -> None:
         """Prepares the producer for exit by cleaning up the producer"""
         self.turnstile.close()
         super(Station, self).close()
